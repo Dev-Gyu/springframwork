c33d22c0873e09024d928ec1c77b4e2785211969
Dev-Gyu <72545045+Dev-Gyu@users.noreply.github.com>
2020-10-22 21:51:21 +0900

    Add files via upload
    Spring Web MVC이용하여 Oracle hr table의 총 사원 수를 Count하려 했으나 empController Bean생성이 되지않는 에러가 발생했다. 하루종일 이 오류와 싸웠지만 해결되지 않아 좀 더 찾아보고 해결해야겠다.

---
+++ SpringProject/src/main/java/com/coderby/myapp/employees/controller/EmpController.java
@@ -0,0 +1,27 @@
+package com.coderby.myapp.employees.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.coderby.myapp.employees.service.IempService;
+
+@Controller
+public class EmpController {
+
+	@Autowired
+	IempService empService;
+	
+	@RequestMapping(value="/hr/")
+	public String empCount(@RequestParam(value="deptid", required=false, defaultValue="0") int deptid, Model model) {
+		if(deptid==0) {
+			model.addAttribute("count", empService.getEmpCount());
+		}else {
+			model.addAttribute("count", empService.getEmpCount(deptid));
+		}
+		return "hr/count";
+	}
+
+}
