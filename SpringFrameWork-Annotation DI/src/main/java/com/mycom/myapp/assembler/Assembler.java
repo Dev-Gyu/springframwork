1de3a2cdeff01a712787843b8ef1589ef8fe8f39
GyuHyeong <cjsworbehd13@gmail.com>
2020-12-23 01:05:26 +0900

    Add files via upload
    2020-12-23
    학습내용
    1. 스프링프레임워크의 Annotation을 이용한 DI방법, 원리
    2. SpringContext interface 구조 학습
    3. mvnrepository 이용해 메이븐 의존성 추가방법 학습

---
+++ SpringFrameWork-Annotation DI/src/main/java/com/mycom/myapp/assembler/Assembler.java
@@ -0,0 +1,29 @@
+package com.mycom.myapp.assembler;
+
+import com.mycom.myapp.spring.ChangePasswordService;
+import com.mycom.myapp.spring.MemberDao;
+import com.mycom.myapp.spring.MemberRegisterService;
+
+public class Assembler {
+	private MemberDao memberDao;
+	private MemberRegisterService regSvc;
+	private ChangePasswordService pwdSvc;
+	
+	public Assembler() {
+		memberDao = new MemberDao();
+		regSvc = new MemberRegisterService(memberDao);
+		pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao);
+	}
+
+	public MemberDao getMemberDao() {
+		return memberDao;
+	}
+	public MemberRegisterService getMemberRegisterService() {
+		return regSvc;
+	}
+	public ChangePasswordService getChangePasswordService() {
+		return pwdSvc;
+	}
+
+}
